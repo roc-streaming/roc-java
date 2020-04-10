6de032cc7d27c9f597522cd0a6ae4a241cb28b00
Arella Matteo <arella.matteo.95@gmail.com>
2020-04-10 19:05:48 +0200

    Context tests with attached non-closed sender & receiver

--- src/test/java/com/github/rocproject/roc/ContextTest.java
+++ src/test/java/com/github/rocproject/roc/ContextTest.java
@@ -1,5 +1,6 @@
 package com.github.rocproject.roc;
 
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -19,6 +20,34 @@ public class ContextTest {
     public void ContextWithInvalidConfigTest() {
         assertThrows(Exception.class, () -> {
             try (Context context = new Context(new ContextConfig.Builder().maxPacketSize(-1).maxFrameSize(-1).build())) {}
+        });
+    }
+
+    @Test
+    public void ContextCloseWithAttachedSender() {
+        assertThrows(IOException.class, () -> {
+            try (Context context = new Context()) {
+                SenderConfig config = new SenderConfig.Builder(44100,
+                                                            ChannelSet.STEREO,
+                                                            FrameEncoding.PCM_FLOAT)
+                                                            .automaticTiming(1)
+                                                        .build();
+                Sender sender = new Sender(context, config);
+            }
+        });
+    }
+
+    @Test
+    public void ContextCloseWithAttachedReceiver() {
+        assertThrows(IOException.class, () -> {
+            try (Context context = new Context()) {
+                ReceiverConfig config = new ReceiverConfig.Builder(44100,
+                        ChannelSet.STEREO,
+                        FrameEncoding.PCM_FLOAT)
+                        .automaticTiming(1)
+                        .build();
+                Receiver receiver = new Receiver(context, config);
+            }
         });
     }
 }

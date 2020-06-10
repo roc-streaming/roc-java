1cf907ba002d8bcc206e595b9a39850658c8c1c5
Arella Matteo <arella.matteo.95@gmail.com>
2020-06-10 17:13:54 +0200

    added android instrumented tests

--- scripts/travis/android/entrypoint/script.sh
+++ scripts/travis/android/entrypoint/script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
+roc_device_name="roc_device"
+roc_device_image="default"
+
+# create and start avd
+device --name "${roc_device_name}" --image "${roc_device_image}" create
+device --name "${roc_device_name}" start
+
 cd android
 ./gradlew build
+./gradlew cAT

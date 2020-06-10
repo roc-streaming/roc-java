bcbb39d878765771ed3353e6d1d651f11f0b9027
Arella Matteo <arella.matteo.95@gmail.com>
2020-06-10 16:21:07 +0200

    roc build with --disable-soversion flag

--- scripts/travis/android/entrypoint/install.sh
+++ scripts/travis/android/entrypoint/install.sh
@@ -6,11 +6,11 @@ abi=$2
 
 scons -Q clean
 
-# remove soversion from soname (for gradle android plugin)
-LDFLAGS="-Wl,-soname,libroc.so" scons -Q --compiler=clang \
+scons -Q --compiler=clang \
     --host=$host \
     --libdir=${ROC_BASE_DIR}/lib/$abi \
     --incdir=${ROC_BASE_DIR}/include/$abi \
+    --disable-soversion \
     --disable-tools \
     --disable-examples \
     --disable-tests \
@@ -22,15 +22,10 @@ scons -Q --compiler=clang \
     --host=$host \
     --libdir=${ROC_BASE_DIR}/lib/$abi \
     --incdir=${ROC_BASE_DIR}/include/$abi \
+    --disable-soversion \
     --disable-tools \
     --disable-examples \
     --disable-tests \
     --disable-pulseaudio \
     --disable-sox \
     --build-3rdparty=libuv,openfec install
-
-# remove symlink from libroc.so (let gradle android plugin packs full library instead of symlink)
-cd ${ROC_BASE_DIR}/lib/$abi && \
-    target=$(readlink -f libroc.so) && \
-    rm libroc.so && \
-    cp $target libroc.so

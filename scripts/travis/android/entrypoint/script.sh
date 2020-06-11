7661c91b81e7f00495479432b0f52e00d564ad24
Arella Matteo <arella.matteo.95@gmail.com>
2020-06-11 13:25:58 +0200

    docker run with --privileged flag
    androidTest with --info and --stacktrace
    
    added kvm virtualization

--- scripts/travis/android/entrypoint/script.sh
+++ scripts/travis/android/entrypoint/script.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -euxo pipefail
 
+export TERM=dumb
+
 roc_device_name="roc_device"
 roc_device_image="default"
 
 # create and start avd
-device --name "${roc_device_name}" --image "${roc_device_image}" create
+device --name "${roc_device_name}" --image "${roc_device_image}" --api "${API}" create
 device --name "${roc_device_name}" start
+
+avdmanager list avd
 
 cd android
 ./gradlew build
-./gradlew cAT
+./gradlew cAT --info --stacktrace
--- scripts/travis/android/script.sh
+++ scripts/travis/android/script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-docker run -t --rm --env API=$ANDROID_API --env BUILD_TOOLS_VERSION=$ANDROID_BUILD_TOOLS_VERSION \
+docker run -t --rm --privileged --device /dev/kvm --env API=$ANDROID_API --env BUILD_TOOLS_VERSION=$ANDROID_BUILD_TOOLS_VERSION \
     --env NDK_VERSION=$ANDROID_NDK_VERSION --env ROC_BASE_DIR=$ROC_BASE_DIR \
     -v "${PWD}:${PWD}" -v $ROC_BASE_DIR:$ROC_BASE_DIR \
     -v android-sdk:/sdk -w "${PWD}" \

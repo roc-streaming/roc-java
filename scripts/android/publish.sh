4f360e585efe42b7da385df3c9e3ee366b065e68
abushmin <diskbu@yandex.ru>
2021-08-22 11:42:33 +0300

    add comment

--- scripts/android/publish.sh
+++ scripts/android/publish.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 export TERM=dumb
 
 cd android
+# automatically creates staging repository and upload archives to there
 ./gradlew uploadArchives
 # close staging and release repository
 ./gradlew closeAndReleaseRepository

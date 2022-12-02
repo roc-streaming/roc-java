2cddcf2a16f1a2297c5ba41853995aa91ba7e4ac
abushmin <diskbu@yandex.ru>
2022-12-03 00:28:47 +0400

    simplify phantom reference based cleaning mechanism

--- src/main/java/org/rocstreaming/roctoolkit/NativeObject.java
+++ src/main/java/org/rocstreaming/roctoolkit/NativeObject.java
@@ -11,16 +11,6 @@ class NativeObject implements AutoCloseable {
     private final static AutoCloseThread AUTO_CLOSE_THREAD = AutoCloseThread.getInstance();
 
     /**
-     *  Underlying roc object native pointer.
-     */
-    private final long ptr;
-
-    /**
-     *  Destructor method.
-     */
-    private final Destructor destructor;
-
-    /**
      *  Reference to {@link NativeObjectReference}.
      */
     private final NativeObjectReference resource;
@@ -34,24 +24,10 @@ class NativeObject implements AutoCloseable {
      * Construct a <code>NativeObject</code>.
      *
      * @param ptr               native pointer to a roc native object
-     * @param dependsOn         <code>NativeObject</code> dependency for
-     *                          finalization ordering
-     * @param destructor        destructor method for closing <code>NativeObject</code>.
-     */
-    protected NativeObject(long ptr, NativeObject dependsOn, Destructor destructor) {
-        this.ptr = ptr;
-        this.destructor = destructor;
-        this.resource = AUTO_CLOSE_THREAD.add(this, dependsOn);
-    }
-
-    /**
-     * Construct a <code>NativeObject</code>.
-     *
-     * @param ptr               native pointer to a roc native object.
      * @param destructor        destructor method for closing <code>NativeObject</code>.
      */
     protected NativeObject(long ptr, Destructor destructor) {
-        this(ptr, null, destructor);
+        this.resource = AUTO_CLOSE_THREAD.add(this, ptr, destructor);
     }
 
     /**
@@ -61,16 +37,11 @@ class NativeObject implements AutoCloseable {
      *                          <code>NativeObject</code>.
      */
     long getPtr() {
-        return this.ptr;
+        return this.resource.getPtr();
     }
 
-    /**
-     * Get <code>NativeObject</code> destructor.
-     *
-     * @return                  destructor method for closing <code>NativeObject</code>.
-     */
-    Destructor getDestructor() {
-        return this.destructor;
+    public NativeObjectReference getResource() {
+        return resource;
     }
 
     /**

b880a45422c3c1c2514153f84b11faa2cf221a72
abushmin <diskbu@yandex.ru>
2022-12-23 22:23:32 +0400

    use last compatible 0.1.x roc-toolkit

--- scripts/linux/build_roc.sh
+++ scripts/linux/build_roc.sh
@@ -2,5 +2,10 @@
 set -euxo pipefail
 
 git clone https://github.com/roc-streaming/roc-toolkit.git /tmp/roc
+work_dir=$(pwd)
+cd /tmp/roc
+git checkout -q 863a0227b78464c3a56fc0484bec73c891e4b7a8
+cd $work_dir
+
 scons -C /tmp/roc -Q --compiler=gcc --build-3rdparty=openfec
 sudo scons -C /tmp/roc -Q --compiler=gcc --build-3rdparty=openfec install

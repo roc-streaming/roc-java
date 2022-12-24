e8c0d34f342ea6880ea9b8245b30db89c15c0e6f
abushmin <diskbu@yandex.ru>
2022-12-24 12:03:15 +0400

    fix clone roc-toolkit

--- scripts/linux/build_roc.sh
+++ scripts/linux/build_roc.sh
@@ -5,6 +5,7 @@ git clone --recurse-submodules https://github.com/roc-streaming/roc-toolkit.git 
 work_dir=$(pwd)
 cd /tmp/roc
 git checkout -q 863a0227b78464c3a56fc0484bec73c891e4b7a8
+git submodule update --init --recursive
 cd $work_dir
 
 scons -C /tmp/roc -Q --compiler=gcc --build-3rdparty=openfec

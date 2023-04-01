b795668bee217a82189a30ae07975b690bb124ed
Victor Gaydov <victor@enise.org>
2023-04-01 18:13:52 +0400

    Use pragma once

--- roc_jni/src/main/headers/endpoint.h
+++ roc_jni/src/main/headers/endpoint.h
@@ -1,12 +1,9 @@
-#include <jni.h>
+#pragma once
 
-#ifndef ENDPOINT_H_
-#define ENDPOINT_H_
+#include <jni.h>
 
 #include <roc/endpoint.h>
 
 int endpoint_unmarshal(JNIEnv* env, roc_endpoint** endpoint, jobject jendpoint);
 
 void endpoint_set_port(JNIEnv* env, jobject endpoint, int port);
-
-#endif /* ENDPOINT_H_ */
--- roc_jni/src/main/headers/packet_encoding.h
+++ roc_jni/src/main/headers/packet_encoding.h
@@ -1,7 +1,6 @@
-#include <jni.h>
+#pragma once
 
-#ifndef PACKET_ENCODING_H_
-#define PACKET_ENCODING_H_
+#include <jni.h>
 
 #include "common.h"
 
@@ -10,5 +9,3 @@
 #define PACKET_ENCODING_CLASS PACKAGE_BASE_NAME "/PacketEncoding"
 
 roc_packet_encoding get_packet_encoding(JNIEnv* env, jobject jpacket_encoding);
-
-#endif /* PACKET_ENCODING_H_ */

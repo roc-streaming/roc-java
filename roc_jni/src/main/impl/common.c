557c4030bf3969be17536e3aa1a24a19742a77aa
Victor Gaydov <victor@enise.org>
2023-04-08 15:47:14 +0400

    Add more assertions

--- roc_jni/src/main/impl/common.c
+++ roc_jni/src/main/impl/common.c
@@ -4,66 +4,106 @@
 
 int get_boolean_field_value(
     JNIEnv* env, jclass clazz, jobject obj, const char* attr_name, int* error) {
+    assert(env != NULL);
+    assert(clazz != NULL);
+
     jfieldID attrId = (*env)->GetFieldID(env, clazz, attr_name, "Z");
     assert(attrId != NULL);
+
     return (*env)->GetBooleanField(env, obj, attrId) == JNI_TRUE;
 }
 
 int get_int_field_value(JNIEnv* env, jclass clazz, jobject obj, const char* attr_name, int* error) {
+    assert(env != NULL);
+    assert(clazz != NULL);
+    assert(attr_name != NULL);
+    assert(error != NULL);
+
     jfieldID attrId = (*env)->GetFieldID(env, clazz, attr_name, "I");
     assert(attrId != NULL);
+
     jint ret = (*env)->GetIntField(env, obj, attrId);
     if (ret < INT_MIN || ret > INT_MAX) {
         *error = -1;
         return 0;
     }
+
     return (int) ret;
 }
 
 unsigned int get_uint_field_value(
     JNIEnv* env, jclass clazz, jobject obj, const char* attr_name, int* error) {
+    assert(env != NULL);
+    assert(clazz != NULL);
+    assert(attr_name != NULL);
+    assert(error != NULL);
+
     jfieldID attrId = (*env)->GetFieldID(env, clazz, attr_name, "I");
     assert(attrId != NULL);
+
     jint ret = (*env)->GetIntField(env, obj, attrId);
     if (ret < 0 || (unsigned int) ret > UINT_MAX) {
         *error = -1;
         return 0;
     }
+
     return (unsigned int) ret;
 }
 
 long long get_llong_field_value(
     JNIEnv* env, jclass clazz, jobject obj, const char* attr_name, int* error) {
+    assert(env != NULL);
+    assert(clazz != NULL);
+    assert(attr_name != NULL);
+    assert(error != NULL);
+
     jfieldID attrId = (*env)->GetFieldID(env, clazz, attr_name, "J");
     assert(attrId != NULL);
+
     jlong ret = (*env)->GetLongField(env, obj, attrId);
     assert(sizeof(long long) == sizeof(jlong));
+
     return (long long) ret;
 }
 
 unsigned long long get_ullong_field_value(
     JNIEnv* env, jclass clazz, jobject obj, const char* attr_name, int* error) {
+    assert(env != NULL);
+    assert(clazz != NULL);
+    assert(attr_name != NULL);
+    assert(error != NULL);
+
     jfieldID attrId = (*env)->GetFieldID(env, clazz, attr_name, "J");
     assert(attrId != NULL);
+
     jlong ret = (*env)->GetLongField(env, obj, attrId);
     if (ret < 0) {
         *error = -1;
         return 0LL;
     }
+
     return (unsigned long long) ret;
 }
 
 int get_enum_value(JNIEnv* env, jclass clazz, jobject enumObj) {
+    assert(env != NULL);
+    assert(clazz != NULL);
+
     if (enumObj != NULL) {
         jfieldID attrId = (*env)->GetFieldID(env, clazz, "value", "I");
         return (*env)->GetIntField(env, enumObj, attrId);
     }
+
     return 0;
 }
 
 jobject get_object_field(
     JNIEnv* env, jclass clazz, jobject obj, const char* attr_name, const char* attr_class_name) {
+    assert(env != NULL);
+    assert(clazz != NULL);
+
     jfieldID attrId = (*env)->GetFieldID(env, clazz, attr_name, attr_class_name);
     assert(attrId != NULL);
+
     return (*env)->GetObjectField(env, obj, attrId);
 }

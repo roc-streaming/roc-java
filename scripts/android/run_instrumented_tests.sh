d7f218ab5f60f797d6f4b1cb8348e350035b1487
abushmin <diskbu@yandex.ru>
2023-04-08 20:48:28 +0400

    fix multicast tests on android

--- scripts/android/run_instrumented_tests.sh
+++ scripts/android/run_instrumented_tests.sh
@@ -3,6 +3,12 @@ set -euxo pipefail
 
 export TERM=dumb
 
+# add route for multicast traffic
+nifaces=( $(adb shell "ip a" | grep -Po "[0-9]+: \K([0-9a-z]+)(?=.*state UP)") )
+for niface in "${nifaces[@]}"; do
+    adb shell "su 0 ip route add 224.0.0.0/4 dev ${niface} table local" || continue
+done
+
 cd android
 
 ./gradlew cAT --info --stacktrace || \

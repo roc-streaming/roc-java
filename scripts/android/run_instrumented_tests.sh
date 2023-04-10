3ca7eac28664aeef164a8e1f7514e29fbaf63bc1
abushmin <diskbu@yandex.ru>
2023-04-10 23:19:43 +0400

    test

--- scripts/android/run_instrumented_tests.sh
+++ scripts/android/run_instrumented_tests.sh
@@ -8,7 +8,7 @@ adb shell "ip r"
 # add route for multicast traffic
 nifaces=( $(adb shell "ip a" | grep 'state UP' | cut -d: -f2 | awk '{print $1}') )
 for niface in "${nifaces[@]}"; do
-    adb shell "su 0 ip route add 224.0.0.0/4 dev ${niface} table local" || continue
+    adb shell "su 0 ip route add 224.0.0.0/4 dev ${niface}" || continue
 done
 
 adb shell "ip a"

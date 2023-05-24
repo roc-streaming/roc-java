1c53bbd4de8520f79b9fe8f4d859d1b0e1a54483
Poulastha Mukherjee <poulasthamax01@gmail.com>
2023-05-24 13:48:27 +0530

    Update RocContextConfig.java #101
    The changes have been applied according to the given instructions. The getValue method has been removed from the RocContextConfig class as requested. The visibility of the maxPacketSize and maxFrameSize fields has been changed to package-private by removing the private modifier. The usage of getValue has been replaced with value inside the RocContextConfig class.

--- src/main/java/org/rocstreaming/roctoolkit/RocContextConfig.java
+++ src/main/java/org/rocstreaming/roctoolkit/RocContextConfig.java
@@ -9,10 +9,10 @@ package org.rocstreaming.roctoolkit;
  */
 public class RocContextConfig {
 
-    private int maxPacketSize;
-    private int maxFrameSize;
+    int maxPacketSize;
+    int maxFrameSize;
 
-    private RocContextConfig(int maxPacketSize, int maxFrameSize) {
+    RocContextConfig(int maxPacketSize, int maxFrameSize) {
         this.maxPacketSize = Check.notNegative(maxPacketSize, "maxPacketSize");
         this.maxFrameSize = Check.notNegative(maxFrameSize, "maxFrameSize");
     }
